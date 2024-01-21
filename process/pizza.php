51d5de1235e8aeb3b5c2b4444e61338f5e63c2fa
paulorobertorodrigues <https://github.com/paulorobertorodrigues/marketplace.git>
2024-01-21 13:06:14 -0300

    css dashboard

--- process/pizza.php
+++ process/pizza.php
@@ -37,9 +37,46 @@
 
         } else {
 
-            echo "passou da validação";
-            exit;
+           //Salvando borda e massa na pizza
+           $stmt = $conn->prepare("INSERT INTO pizzas (borda_id, massa_id) VALUES (:borda, :massa)");
 
+           //filtrando inputs
+           $stmt->bindParam(":borda", $borda, PDO::PARAM_INT);
+           $stmt->bindParam(":massa", $massa, PDO::PARAM_INT);
+
+           $stmt->execute();
+
+           //resgatando ultimo id da ultima pizza
+           $pizzaId = $conn->lastInsertId();
+
+           $stmt = $conn->prepare("INSERT INTO pizza_sabor (pizza_id, sabor_id) VALUES (:pizza, :sabor)");
+           
+           //repetição até terminar de salvar todos os sabores
+           foreach($sabores as $sabor) {
+            //filtrando os inputs
+            $stmt->bindParam(":pizza", $pizzaId, PDO::PARAM_INT);
+            $stmt->bindParam(":sabor", $sabor, PDO::PARAM_INT);
+
+            $stmt->execute();
+
+           }
+
+           //criar o pedido da pizza
+           $stmt = $conn->prepare("INSERT INTO pedidos (pizza_id, status_id) VALUES (:pizza, :status)");
+
+           //status -> sempre inicia com 1, que é em produção
+           $statusId = 1;
+
+           //filtrar inputs
+           $stmt->bindParam(":pizza", $pizzaId);
+           $stmt->bindParam(":status", $statusId);
+
+           $stmt->execute();
+
+           //exibir mensagem de sucesso
+           $_SESSION["msg"] = "Pedido realizado com sucesso";
+           $_SESSION["status"] = "success";
+           
         }
         //Retorna para pagina inicial
     header("Location:..");
