0a19fce81e70bfcef92e77bd9a117b35af831215
paulorobertorodrigues <https://github.com/paulorobertorodrigues/marketplace.git>
2024-01-20 22:38:59 -0300

    resgatando dados da home

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     include_once("templates/header.php");
+    include_once("process/pizza.php");
 ?>
 
     <div id="main-banner">
@@ -15,17 +16,26 @@
                             <label for="borda">Borda:</label>
                             <select name="borda" id="borda" class="form-control">
                                 <option value="">Selecione a borda</option>
+                                <?php foreach($bordas as $borda): ?>
+                                    <option value="<?=$borda["id"]?>"><?=$borda["tipo"]?></option>
+                                    <?php endforeach;?>
                             </select>
                         </div>
                         <div class="form-group">
                             <label for="massa">Massa:</label>
                             <select name="massa" id="massa" class="form-control">
                                 <option value="">Selecione a massa</option>
+                                <?php foreach($massas as $massa): ?>
+                                    <option value="<?=$massa["id"]?>"><?=$massa["tipo"]?></option>
+                                    <?php endforeach;?>
                             </select>
                         </div>
                         <div class="form-group">
                             <label for="sabores">Sabores: (Máximo 3)</label>
                             <select multiple name="sabores[]" id="sabores" class="form-control">
+                            <?php foreach($sabores as $sabor): ?>
+                                    <option value="<?=$sabor["id"]?>"><?=$sabor["nome"]?></option>
+                                    <?php endforeach;?>
                                 
                             </select>
                         </div>
---
+++ process/pizza.php
@@ -0,0 +1,28 @@
+<?php
+
+    include_once("conn.php");
+
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    // Resgate dos dados, montagem do pedido
+    if($method === "GET") {
+
+        $bordasQuery = $conn->query("SELECT * FROM bordas;");
+        $bordas = $bordasQuery->fetchAll();
+
+        
+        $massasQuery = $conn->query("SELECT * FROM massas;");
+        $massas = $massasQuery->fetchAll();
+
+        
+        $saboresQuery = $conn->query("SELECT * FROM sabores;");
+        $sabores = $saboresQuery->fetchAll();
+
+       
+
+        //criação do pedido
+    } else if ($method === "POST") {
+
+    }
+
+?>
