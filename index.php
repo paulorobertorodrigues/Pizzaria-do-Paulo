d810829f495f4b9d582d4e466e52f57f882898f5
paulorobertorodrigues <https://github.com/paulorobertorodrigues/marketplace.git>
2024-01-20 21:30:41 -0300

    inicio header

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    include_once("templates/header.php");
+?>
+
+    <h1>Testando CSS</h1>
+    <i class="fas fa-sync-alt"></i>
+
+ <?php
+    include_once("templates/footer.php");
+ ?>
---
+++ templates/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faça seu Pedido!</title>
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--App CSS-->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg">
+            <a href="index.php" class="navbar-brand">
+                <img src="img/pizzasvg.png" alt="Pizzaria do Paulo" id="brand-logo">
+            </a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a href="index.php" class="nav-link">Peça sua pizza</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
