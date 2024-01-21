75dddba5aab1f889e6c6abb996eba55149e4148b
paulorobertorodrigues <https://github.com/paulorobertorodrigues/marketplace.git>
2024-01-20 22:06:06 -0300

    finalizado front-end

--- index.php
+++ index.php
@@ -2,8 +2,41 @@
     include_once("templates/header.php");
 ?>
 
-    <h1>Testando CSS</h1>
-    <i class="fas fa-sync-alt"></i>
+    <div id="main-banner">
+        <h1>Faça seu Pedido</h1>
+    </div>
+    <div id="main-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Monte a pizza como desejas:</h2>
+                    <form action="process/pizza.php" method="POST" id="pizza-form">
+                        <div class="form-group">
+                            <label for="borda">Borda:</label>
+                            <select name="borda" id="borda" class="form-control">
+                                <option value="">Selecione a borda</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="massa">Massa:</label>
+                            <select name="massa" id="massa" class="form-control">
+                                <option value="">Selecione a massa</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="sabores">Sabores: (Máximo 3)</label>
+                            <select multiple name="sabores[]" id="sabores" class="form-control">
+                                
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" value="Fazer Pedido!">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
  <?php
     include_once("templates/footer.php");
